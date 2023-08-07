2239f0b64c7c62e63a29989df3ad74561510cd44
abdou idaala <abdou@idaala.com>
2023-08-07 10:23:02 -0700

    update

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+length = len(word)
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[length - 2:]}")
+print(f"Middle word: {word[1:length - 1]}")
