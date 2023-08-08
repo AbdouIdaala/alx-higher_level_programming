1afa743a8beb57172327864a4c926e711b12983f
abdou idaala <abdou@idaala.com>
2023-08-08 21:14:29 +0100

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    text = ""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            text += chr(ord(c) - 32)
+    print(text)
+
+
+uppercase("best")
