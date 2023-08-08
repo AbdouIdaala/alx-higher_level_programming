821c29d590ee91c57187fa4d552ebe0c2c4584fe
abdou idaala <abdou@idaala.com>
2023-08-08 13:53:54 +0100

    00...99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number != 99 and number < 10:
+        print("{:02}, ".format(number), end='')
+    elif number != 99 and number >= 10:
+        print("{}, ".format(number), end='')
+    else:
+        print(number)
