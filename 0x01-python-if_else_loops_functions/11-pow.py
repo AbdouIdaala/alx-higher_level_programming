2a0421f374de1bcee437e5b022f74ec93cd38d0f
abdou idaala <abdou@idaala.com>
2023-08-08 22:37:48 +0100

    a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
     result = 1
-    for i in range(b):
-        result *= a
+    if b > 0:
+        for i in range(b):
+            result *= a
+    else:
+        i = b
+        while i < 0:
+            result /= 10
+            i += 1
     return result
