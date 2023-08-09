392688c1f24b3df029f58a3a0bede5180985ce87
abdou idaala <abdou@idaala.com>
2023-08-09 19:12:51 +0100

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,21 +10,13 @@ int check_cycle(listint_t *list)
 {
 	listint_t *temp1 = list;
 	listint_t *temp2 = NULL;
-	listint_t *temp3 = NULL;
 
 	if (temp1)
 	{
-		temp3 = list;
-		while (temp3)
-		{
-			if (temp3 == list)
-				return (1);
-			temp3 = temp3->next;
-		}
 		temp2 = temp1->next;
 		while (temp2)
 		{
-			if (temp2 == temp1 || temp2 == temp1->next)
+			if (temp2 == list || temp2 == temp1)
 				return (1);
 			temp1 = temp1->next;
 			temp2 = temp2->next;
