94ae3410ac74b2e27e50857c9459b68b7bb18acc
abdou idaala <abdou@idaala.com>
2023-08-13 07:40:36 +0100

    Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = ""
+    for c in my_string:
+        if c == 'c' or c == 'C':
+            continue
+        else:
+            new_string += c
+    return new_string
+
+
+print(no_c("Best School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
