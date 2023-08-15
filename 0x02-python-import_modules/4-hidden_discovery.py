88cbba2e2d267d6c84ca2f20ddb9a54fa5fa6036
abdou idaala <abdou@idaala.com>
2023-08-15 22:03:49 +0100

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+def helper():
+    with open("./hidden_4.pyc", 'rb') as f:
+        return f.read()
+
+
 if __name__ == "__main__":
-    file = open("./hidden_4.pyc", 'rb').read()
-    print(str(file, encoding='latin-1'))
+    print(helper())
