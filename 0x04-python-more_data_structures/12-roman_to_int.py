44c15614b01a98414b9f511a9dd03ee9ea320d7a
abdou idaala <abdou@idaala.com>
2023-08-16 18:37:38 +0100

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -44,7 +44,7 @@ def dictionary():
 
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string == None:
+    if not isinstance(roman_string, str):
         return 0
     reference = dictionary()
     result = 0
