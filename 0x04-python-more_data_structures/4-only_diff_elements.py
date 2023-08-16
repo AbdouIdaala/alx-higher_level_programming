be59c58fdf3f1ba5e80d585b2d62c39783d46cd7
abdou idaala <abdou@idaala.com>
2023-08-16 14:32:35 +0100

    Only differnts

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    new_set = set_1.union(set_2)
+    new_set = (set_1.union(set_2)).difference()
     return new_set
+
+
+set_1 = {"Python", "C", "Javascript"}
+set_2 = {"Bash", "C", "Ruby", "Perl"}
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
