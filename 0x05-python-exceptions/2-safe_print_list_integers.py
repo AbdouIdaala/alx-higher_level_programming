67448fbfbc8e986f434c6f706cfa31b213ba8e0c
abdou idaala <abdou@idaala.com>
2023-08-30 20:22:42 +0100

    Print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = length = 0
+    count = 0
     try:
         for i in range(x):
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end='')
                 count += 1
-            length += 1
-    except IndexError:
-        return length
-    else:
         return count
     finally:
         print()
