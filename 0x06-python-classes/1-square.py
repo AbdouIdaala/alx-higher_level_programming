b5871c8170597b3f7cd2ffc5adfbf8ac944cbcd0
abdou idaala <abdou@idaala.com>
2023-08-30 21:12:43 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square with size
+"""
+
+
+class Square:
+    """A class Square that defines a square
+    """
+
+    def __init__(self, size):
+        self.__size = size
+        pass
