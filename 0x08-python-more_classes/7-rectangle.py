461a74dc63fb11c5421e229600c6e8f55ae63aaf
abdou idaala <abdou@idaala.com>
2023-09-04 22:34:39 +0100

    task-7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -100,7 +100,7 @@ class Rectangle:
         else:
             for i in range(self.__height):
                 for j in range(self.__width):
-                    full_str += self.print_symbol
+                    full_str += Rectangle.print_symbol
                 full_str += '\n'
         return full_str
 
