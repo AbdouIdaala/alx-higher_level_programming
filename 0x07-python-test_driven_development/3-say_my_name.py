bdc09025247b9036d7c9bca1dc588d6e94a6f371
abdou idaala <abdou@idaala.com>
2023-09-05 19:40:47 +0100

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 def say_my_name(first_name, last_name=""):
+    """Print My name is <first name> <last name>
+
+    Args:
+        first_name (str): mandatory
+        last_name (str, optional): Defaults to "".
+
+    Raises:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
