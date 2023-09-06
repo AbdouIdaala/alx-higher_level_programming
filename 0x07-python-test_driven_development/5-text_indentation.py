98dae65077f2a1c90d0930840a6baa0f93fe72c8
abdou idaala <abdou@idaala.com>
2023-09-06 11:53:15 +0100

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""_summary_
+"""
+
 
 def text_indentation(text):
+    """Print a text with 2 new lines after each '.', '?' and ':'
+
+    Args:
+        text (str): the text to modify
+
+    Raises:
+        TypeError: text must be a string
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     copy = ""
