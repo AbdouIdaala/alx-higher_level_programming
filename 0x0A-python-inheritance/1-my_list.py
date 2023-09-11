5d335a1ac7c41e40ee019e199fc0bb9eec35333a
abdou idaala <abdou@idaala.com>
2023-09-11 13:28:03 +0100

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""My list
+"""
+
+
+class MyList(list):
+    """_summary_
+
+    Args:
+        list (_type_): _description_
+    """
+
+    def print_sorted(self):
+        """_summary_
+        """
+        self.new_list = []
+        self.new_list.extend(self)
+        self.new_list.sort()
+        print(self.new_list)
