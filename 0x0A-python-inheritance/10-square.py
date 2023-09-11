5221690e7af9bb49e59ec06a98a5df108334a7b4
abdou idaala <abdou@idaala.com>
2023-09-11 22:04:12 +0100

    Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""import module
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+"""_summary_
+"""
+
+
+class Square(Rectangle):
+    """_summary_
+
+    Args:
+        Rectangle (_type_): _description_
+    """
+
+    def __init__(self, size):
+        """_summary_
+
+        Args:
+            size (_type_): _description_
+        """
+        Rectangle.__init__(self, size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
