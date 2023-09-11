4bd98e4b1573e164e0e7f42e96ace7bcc9520d03
abdou idaala <abdou@idaala.com>
2023-09-11 21:31:03 +0100

    Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""_summary_
+"""
+
+
+class Rectangle(BaseGeometry):
+    """_summary_
+
+    Args:
+        BaseGeometry (_type_): _description_
+    """
+
+    def __init__(self, width, height):
+        """_summary_
+
+        Args:
+            width (_type_): _description_
+            height (_type_): _description_
+        """
+        self.__width = BaseGeometry.integer_validator(self, 'width', width)
+        self.__height = BaseGeometry.integer_validator(self, 'height', height)
