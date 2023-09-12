b952c9d1f1c0a297e5af51e23f42782bf4424720
abdou idaala <abdou@idaala.com>
2023-09-12 19:01:00 +0100

    Save Object to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,6 +11,6 @@ def save_to_json_file(my_obj, filename):
         my_obj (_type_): _description_
         filename (_type_): _description_
     """
-    json_str = json.dumps(my_obj, indent=2)
+    json_str = json.dumps(my_obj, indent=4)
     with open(filename, mode='w', encoding='UTF-8') as f:
         f.write(json_str)
