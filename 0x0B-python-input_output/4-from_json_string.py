d85320af9e6669e6dad9b9a292a28b9cc35a0cca
abdou idaala <abdou@idaala.com>
2023-09-12 18:47:20 +0100

    From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""import json module
+"""
+import json
+
+
+def from_json_string(my_str):
+    """_summary_
+
+    Args:
+        my_str (_type_): _description_
+    """
+    return my_str
