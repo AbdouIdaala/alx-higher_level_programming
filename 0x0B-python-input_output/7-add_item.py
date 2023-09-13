980e81c66bfc31db759815e5617052ebe8f353ca
abdou idaala <abdou@idaala.com>
2023-09-13 16:45:48 +0100

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,3 +11,8 @@ if "add_item.json" in os.listdir():
     for i in range(1, len(sys.argv)):
         loaded_file.append(sys.argv[i])
     save_to_json_file(loaded_file, "add_item.json")
+else:
+    my_list = []
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
+    save_to_json_file(my_list, "add_item.json")
