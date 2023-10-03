3d08a5d4d2d3a17692cb59048ccbdd9e5c25b56b
abdou idaala <abdou@idaala.com>
2023-10-03 10:08:31 +0100

    remove a directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""_summary_
-"""
-
-
-def lookup(obj):
-    """Return the list of available attributes and methods of an object
-    """
-    return dir(obj)
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""_summary_
-"""
-
-
-class BaseGeometry:
-    """_summary_
-    """
-    pass
